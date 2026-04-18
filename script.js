7c387a4438266ee17537d4ed1bc7dfdd464a11f1
ksenia-25-fp <226387090+ksenia-25-fp@users.noreply.github.com>
2026-04-18 17:39:49 +0200

    Add pseudocode for getComputerChoice logic

---
+++ script.js
@@ -0,0 +1,8 @@
+function getComputerChoice (){
+
+//use Math.random to get the value and save it to a variable called randomNum
+
+// if randomNum is less then 0.33 return "rock"
+// else if randomNum less then 0.66 return "paper"
+// else return "scissors"
+}
