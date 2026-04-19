a2038c2d097b17b87b60185a68dc515f5ee60b91
ksenia-25-fp <226387090+ksenia-25-fp@users.noreply.github.com>
2026-04-19 15:14:55 +0200

    Implement playRound and connect game logic
    Add logic for win/loss/tie conditions, update scores,
    and integrate human and computer selection functions.

--- script.js
+++ script.js
@@ -11,7 +11,31 @@ if (randomNum < 0.33) {
 }
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getHumanChoice (){
 let choice = prompt ("Enter rock, paper or scissors");
+choice = choice.toLowerCase();
 return choice;
 }
+
+
+function playRound (humanChoice, computerChoice) {
+if (humanChoice === computerChoice) {
+    console.log ("A draw. You’re not so easy to beat!")
+} else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+    console.log ("You win!")
+    humanScore ++; 
+} else {
+    console.log ("You lose! Hahahaha")
+    computerScore ++;
+} 
+console.log (humanScore, computerScore);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
