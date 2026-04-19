be5431105bed7bbef957c50cccbf0925ad61c12c
ksenia-25-fp <226387090+ksenia-25-fp@users.noreply.github.com>
2026-04-19 16:11:50 +0200

    Implement playGame with 5-round loop & final score
    Refactor logic into a central function including score tracking,
    nested playRound, and final winner announcement

--- script.js
+++ script.js
@@ -4,28 +4,33 @@ let randomNum = Math.random();
 
 if (randomNum < 0.33) {
     return "rock";
-} else if (randomNum < 0.66){
+} else if (randomNum < 0.66) {
     return "paper";
 } else {
     return "scissors";
 }
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
-
-function getHumanChoice (){
+function getHumanChoice () {
 let choice = prompt ("Enter rock, paper or scissors");
 choice = choice.toLowerCase();
 return choice;
 }
 
 
+function playGame () {
+
+let humanScore = 0;
+let computerScore = 0;
+
+
 function playRound (humanChoice, computerChoice) {
 if (humanChoice === computerChoice) {
     console.log ("A draw. You’re not so easy to beat!")
-} else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+} else if ((humanChoice === "rock" && computerChoice === "scissors") || 
+(humanChoice === "paper" && computerChoice === "rock") || 
+(humanChoice === "scissors" && computerChoice === "paper")) {
     console.log ("You win!")
     humanScore ++; 
 } else {
@@ -35,7 +40,22 @@ if (humanChoice === computerChoice) {
 console.log (humanScore, computerScore);
 }
 
+
+for (let i = 0; i < 5; i++) {
+
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
+}
+
+if (humanScore > computerScore) {
+    alert ("You are the ultimate champion!")
+} else if (computerScore > humanScore) {
+    alert ("Machine uprising is here. You lost the game.")
+} else {
+    alert ("A total draw!")
+}
+}
+
+playGame();
