3bd0af26a8411e2a0f6e039ccf0f997392258008
ksenia-25-fp <226387090+ksenia-25-fp@users.noreply.github.com>
2026-04-20 15:51:56 +0200

    Refine spacing

--- script.js
+++ script.js
@@ -1,61 +1,54 @@
-function getComputerChoice (){
+function getComputerChoice () {
+    let randomNum = Math.random();
 
-let randomNum = Math.random();
-
-if (randomNum < 0.33) {
-    return "rock";
-} else if (randomNum < 0.66) {
-    return "paper";
-} else {
-    return "scissors";
+    if (randomNum < 0.33) {
+        return "rock";
+    } else if (randomNum < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
-}
-
 
 function getHumanChoice () {
-let choice = prompt ("Enter rock, paper or scissors");
-choice = choice.toLowerCase();
-return choice;
+    let choice = prompt ("Enter rock, paper or scissors");
+    choice = choice.toLowerCase();
+    return choice;
 }
-
 
 function playGame () {
+    let humanScore = 0;
+    let computerScore = 0;
 
-let humanScore = 0;
-let computerScore = 0;
+    function playRound (humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log ("A draw. You’re not so easy to beat!")
+        } else if ((humanChoice === "rock" && computerChoice === "scissors") || 
+        (humanChoice === "paper" && computerChoice === "rock") || 
+        (humanChoice === "scissors" && computerChoice === "paper")) {
+            console.log ("You win!")
+            humanScore ++; 
+        } else {
+            console.log ("You lose! Hahahaha")
+            computerScore ++;
+        } 
+        console.log (humanScore, computerScore);
+    }
 
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
 
-function playRound (humanChoice, computerChoice) {
-if (humanChoice === computerChoice) {
-    console.log ("A draw. You’re not so easy to beat!")
-} else if ((humanChoice === "rock" && computerChoice === "scissors") || 
-(humanChoice === "paper" && computerChoice === "rock") || 
-(humanChoice === "scissors" && computerChoice === "paper")) {
-    console.log ("You win!")
-    humanScore ++; 
-} else {
-    console.log ("You lose! Hahahaha")
-    computerScore ++;
-} 
-console.log (humanScore, computerScore);
-}
+        playRound(humanSelection, computerSelection);
+    }
 
-
-for (let i = 0; i < 5; i++) {
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-}
-
-if (humanScore > computerScore) {
-    alert ("You are the ultimate champion!")
-} else if (computerScore > humanScore) {
-    alert ("Machine uprising is here. You lost the game.")
-} else {
-    alert ("A total draw!")
-}
+    if (humanScore > computerScore) {
+        alert ("You are the ultimate champion!")
+    } else if (computerScore > humanScore) {
+        alert ("Machine uprising is here. You lost the game.")
+    } else {
+        alert ("A total draw!")
+    }
 }
 
 playGame();
